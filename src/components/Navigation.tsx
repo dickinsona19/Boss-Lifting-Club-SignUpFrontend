17115c14121383c199957667ce9b1bd39443d5a6
Andrew Dickinson <dickinsona19@yahoo.com>
2025-03-11 01:56:00 -0400

    deploy attempt

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-<nav className="px-2 py-1 flex items-center justify-between border-b border-neutral-200 bg-black text-white fixed top-0 left-0 right-0 z-50">
+<nav className="px-2 py-1 flex items-center justify-between border-b border-neutral-200 bg-black text-white">
         <div className="flex items-center gap-6">
           <Link to="/">
-            <img src="src\Assets\12b121_5346d265dce543d091ba698f4cfd1461~mv2.png" alt="Logo" className="w-20 h-20" />
+            <img src="/12b121_5346d265dce543d091ba698f4cfd1461~mv2.png" alt="Logo" className="w-20 h-20" />
           </Link>
           <div className="hidden md:flex items-center gap-4 text-sm">
           </div>
